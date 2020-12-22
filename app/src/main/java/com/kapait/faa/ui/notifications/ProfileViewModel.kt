6e34531b52b5e468e67ec7b31f72a8758dce3266
NLCodeTeam <nlcodeteam@gmail.com>
2020-12-22 23:08:04 +0300

    init commit

---
+++ app/src/main/java/com/kapait/faa/ui/notifications/ProfileViewModel.kt
@@ -0,0 +1,13 @@
+package com.kapait.faa.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ProfileViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
